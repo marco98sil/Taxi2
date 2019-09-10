ab87f80458a0941b03c0fb21d4d5a382c70670e6
marco98sil <marco98sil@gmail.com>
2019-09-10 17:32:42 +0200

    Initial commit

---
+++ app/src/main/java/android/example/taxi2/MainActivity.java
@@ -0,0 +1,168 @@
+package android.example.taxi2;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView coordinate;
+    TextView coordinate_old;
+    LocationManager locationManager;
+    Context mContext;
+    double distanza = 0;
+    double latitude_old;
+    double longitude_old;
+    Button STOP ;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        coordinate = (TextView) findViewById(R.id.id_textview);
+        coordinate_old = (TextView) findViewById(R.id.id_textview_old);
+        STOP = (Button) findViewById(R.id.button_STOP);
+        mContext = this;
+        locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+
+        Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                2000,
+                10, locationListenerGPS);
+        isLocationEnabled();
+
+        latitude_old = location.getLatitude();
+        longitude_old = location.getLongitude();
+        coordinate_old.setText("Old Latitude: "+latitude_old + "\n" + "Old Longitude: "+longitude_old);
+    }
+
+
+    LocationListener locationListenerGPS=new LocationListener() {
+        @Override
+        public void onLocationChanged(android.location.Location location) {
+            double latitude=location.getLatitude();
+            double longitude=location.getLongitude();
+            //String msg="New Latitude: "+latitude + "New Longitude: "+longitude;
+           // Toast.makeText(mContext,msg,Toast.LENGTH_LONG).show();
+            coordinate.setText("New Latitude: "+latitude + "\n" + "New Longitude: "+longitude);
+            distanza += GetDistanceFromLatLonInKm(latitude, longitude, latitude_old, longitude_old);
+            latitude_old = latitude;
+            longitude_old = longitude;
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    public double GetDistanceFromLatLonInKm(double lat1, double lon1, double lat2, double lon2)
+    {
+        final int R = 6371;
+        // Radius of the earth in km
+        double dLat = deg2rad(lat2 - lat1);
+        // deg2rad below
+        double dLon = deg2rad(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = R * c;
+        // Distance in km
+        return d;
+    }
+    private double deg2rad(double deg)
+    {
+        return deg * (Math.PI / 180);
+    }
+
+    protected void onResume(){
+        super.onResume();
+       // isLocationEnabled();
+    }
+
+    private void isLocationEnabled() {
+
+        if (!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+            alertDialog.setTitle("Enable Location");
+            alertDialog.setMessage("Your locations setting is not enabled. Please enabled it in settings menu.");
+            alertDialog.setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alert = alertDialog.create();
+            alert.show();
+        } else {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+            alertDialog.setTitle("Confirm Location");
+            alertDialog.setMessage("Your Location is enabled, please enjoy");
+            alertDialog.setNegativeButton("Back to interface", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alert = alertDialog.create();
+            alert.show();
+        }
+    }
+
+    public void FERMA(View view) {
+        //locationManager.removeUpdates(locationListenerGPS);
+        //locationManager = null;
+        if(distanza < 0.5) distanza = 0;
+        coordinate.setText("Totale distanza percorsa: "+distanza+" km");
+    }
+
+    public void RIPARTI(View view) {
+        distanza = 0;
+    }
+}
